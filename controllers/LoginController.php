9f9f38adb4a64d8ed688688e3b67d96f60193ee8
Dhamecha Rahul <34760963+rahulsiimteq@users.noreply.github.com>
2018-01-22 09:36:31 +0530

    register

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,11 +1,5 @@
 <?php
 
 require_once ('views/login.php');
-function demo(){
-if(isset($_GET['name'])){
-  $email = $_GET['name'];
-  echo "<p>Welcome $email.</p>";
 
-}
-}
 ?>
--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -1,4 +1,7 @@
 <?php
 
+$getMethod = $_GET['getMethod'];
+
 require_once ('views/register.php');
+
 ?>
